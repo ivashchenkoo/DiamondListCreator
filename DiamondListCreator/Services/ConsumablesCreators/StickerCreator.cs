3035d3cb31be925ad78c1c45174268c3f01fed6f
Oleksandr <alexander.ivashchenkoo@gmail.com>
2022-07-10 14:44:39 +0300

    Added stickers creation

--- DiamondListCreator/Services/ConsumablesCreators/StickerCreator.cs
+++ DiamondListCreator/Services/ConsumablesCreators/StickerCreator.cs
@@ -1,18 +1,55 @@
 ﻿using DiamondListCreator.Models;
-using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
 
 namespace DiamondListCreator.Services.ConsumablesCreators
 {
     public class StickerCreator
     {
-        public StickerCreator()
-        {
+        private readonly PrivateFontCollection pfc;
 
+        public StickerCreator(PrivateFontCollection pfc)
+        {
+            this.pfc = pfc;
+        }
+
+        ~StickerCreator()
+        {
+            pfc.Dispose();
+        }
+
+        /// <summary>
+        /// Creating page with stikers from diamonds list
+        /// </summary>
+        /// <returns>Array of Bitmaps with created stickers pages</returns>
+        public Bitmap[] CreateStickersPage(List<DiamondSettings> diamonds)
+        {
+            int pageCount = diamonds.Count / 12 != 0 ? diamonds.Count / 12 : 1;
+            Bitmap[] stickersPages = new Bitmap[pageCount];
+
+            float pageWidth = 2480f;
+            float pageHeight = 3507f;
+
+            stickersPages[0] = new Bitmap((int)pageWidth, (int)pageHeight, PixelFormat.Format32bppArgb);
+
+            for (int i = 0, j = 0, p = 0; i < diamonds.Count; i++)
+            {
+                if (i % 12 == 0 && i > 0)
+                {
+                    stickersPages[p] = new Bitmap((int)pageWidth, (int)pageHeight, PixelFormat.Format32bppArgb);
+                    p++;
+                    j = 0;
+                }
+
+                int row = j / 3;
+                int column = j++ % 3;
+
+                stickersPages[p] = AppendStickerOnPage(stickersPages[p], diamonds[i], row, column);
+            }
+
+            return stickersPages;
         }
 
         /// <summary>
@@ -21,16 +58,58 @@ namespace DiamondListCreator.Services.ConsumablesCreators
         /// <returns>Page with appended sticker on specified position</returns>
         public Bitmap AppendStickerOnPage(Bitmap page, DiamondSettings diamond, int row, int column)
         {
-            return null;
-        }
+            using (Graphics graph = GraphicsService.GetGraphFromImage(page))
+            {
+                // Thumbnail
+                Bitmap OverlayBitmap = new Bitmap(diamond.Path + "/Вид вышивки.png");
+                if (OverlayBitmap.Height > OverlayBitmap.Width)
+                {
+                    OverlayBitmap.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                }
+                float OldWidth = OverlayBitmap.Width;
+                float OldHeight = OverlayBitmap.Height;
+                float appendedImageWidth = 808f;
+                float appendedImageHeight = 640f;
+                float newHeight = OldHeight / OldWidth < 0.6 ? appendedImageWidth / (OldWidth / OldHeight) : appendedImageHeight;
+                int osX = 822 * column;
+                int osY = 820 * row;
+                graph.DrawImage(OverlayBitmap, 14 + osX, 6 + osY + (appendedImageHeight - newHeight), appendedImageWidth, newHeight);
+                graph.DrawImage(OverlayBitmap, 14 + osX, 6 + osY + (appendedImageHeight - newHeight), appendedImageWidth, newHeight);
+                graph.DrawImage(OverlayBitmap, 14 + osX, 6 + osY + (appendedImageHeight - newHeight), appendedImageWidth, newHeight);
 
-        /// <summary>
-        /// Appending only 3 sticker on specified row, even if the number of list items is larger
-        /// </summary>
-        /// <returns>Page with appended 3 stickers on specified row</returns>
-        public Bitmap AppendStickersRowOnPage(List<DiamondSettings> diamonds, int row)
-        {
-            return null;
+                SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+                // Name
+                Font font = new Font(pfc.Families[0], 120);
+                SizeF sizef;
+                if (diamond.Name.EndsWith("+"))
+                {
+                    sizef = graph.MeasureString(diamond.Name.Substring(0, diamond.Name.Length - 1), font);
+                    SizeF plusSize = graph.MeasureString("+", new Font(pfc.Families[0], 90));
+                    graph.DrawString(diamond.Name.Substring(0, diamond.Name.Length - 1), font, drawBrush, (((page.Width / 3) - sizef.Width) / 2) + osX - (plusSize.Width / 3f) + 30f, 650 + ((200 - sizef.Height) / 2) + osY);
+                    graph.DrawString("+", new Font(pfc.Families[0], 90), drawBrush, (((page.Width / 3) - sizef.Width) / 2) + osX + sizef.Width - plusSize.Width + 60f, 650 + ((200 - plusSize.Height) / 2) + osY);
+                }
+                else
+                {
+                    sizef = graph.MeasureString(diamond.Name, font);
+                    graph.DrawString(diamond.Name, font, drawBrush, (((page.Width / 3) - sizef.Width) / 2) + osX, 650 + ((200 - sizef.Height) / 2) + osY);
+                }
+
+                // Lines
+                Pen pen = new Pen(drawBrush, 14);
+                graph.DrawLine(pen, osX + 7, osY, osX + 7, 650 + osY);
+                graph.DrawLine(pen, (822 * (column + 1)) + 7, osY, (822 * (column + 1)) + 7, 650 + osY);
+                pen = new Pen(drawBrush, 10);
+                graph.DrawLine(pen, osX, 3 + osY, 822 * (column + 1), 3 + osY);
+                graph.DrawLine(pen, osX, 6 + 640 + osY, 822 * (column + 1), 6 + 640 + osY);
+
+                if (column == 0)
+                {
+                    graph.DrawLine(pen, 0, 3 + (820 * (row + 1)), 2480, 3 + (820 * (row + 1)));
+                }
+            }
+
+            return page;
         }
     }
 }
---
+++ DiamondListCreator/Services/StickersService.cs
@@ -0,0 +1,45 @@
+﻿using DiamondListCreator.Models;
+using DiamondListCreator.Services.ConsumablesCreators;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using Rectangle = iTextSharp.text.Rectangle;
+
+namespace DiamondListCreator.Services
+{
+    public class StickersService
+    {
+        /// <summary>
+        /// Creating Stickers and saving them into pdf /Stickers {Date.Now}.pdf
+        /// </summary>
+        /// <param name="diamonds">DiamondSettings list</param>
+        /// <param name="paths">Path settings</param>
+        public void CreateStickersPdf(List<DiamondSettings> diamonds, PathSettings paths)
+        {
+            MemoryStream stream = new MemoryStream();
+            Rectangle pageSize = new Rectangle(0, 0, 2480, 3507);
+            Document document = new Document(pageSize, 0, 0, 0, 0);
+            _ = PdfWriter.GetInstance(document, stream);
+            document.Open();
+
+            StickerCreator stickerCreator = new StickerCreator(FontCollectionService.InitCustomFont(Properties.Resources.VanishingSizeName_Regular));
+            System.Drawing.Bitmap[] stickers = stickerCreator.CreateStickersPage(diamonds);
+
+            for (int j = stickers.Length - 1; j >= 0; j--)
+            {
+                Image image = Image.GetInstance(stickers[j], ImageFormat.Tiff);
+                _ = document.Add(image);
+            }
+
+            document.Close();
+            byte[] content = stream.ToArray();
+            using (FileStream fs = File.Create(paths.FilesSavePath + "/Stickers " + DateTime.Today.ToString().Substring(0, 10) + ".pdf"))
+            {
+                fs.Write(content, 0, content.Length);
+            }
+        }
+    }
+}
