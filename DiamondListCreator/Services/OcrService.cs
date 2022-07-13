9a375b2d2e8c605e007eef578aad160e71c27b65
Oleksandr <alexander.ivashchenkoo@gmail.com>
2022-07-14 01:41:41 +0300

    Added ocr service

---
+++ DiamondListCreator/Services/OcrService.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+using Patagames.Ocr;
+
+namespace DiamondListCreator.Services
+{
+    public class OcrService
+    {
+        private static OcrApi ocrApi;
+
+        public OcrService(Patagames.Ocr.Enums.Languages language)
+        {
+            ocrApi = OcrApi.Create();
+            ocrApi.Init(language);
+        }
+
+        /// <summary>
+        /// Creates an instance of OcrService class with initializing OcrApi with English language
+        /// </summary>
+        public OcrService()
+        {
+            ocrApi = OcrApi.Create();
+            ocrApi.Init(Patagames.Ocr.Enums.Languages.English);
+        }
+
+        ~OcrService()
+        {
+            ocrApi.Dispose();
+        }
+
+        public string GetTextFromImage(Bitmap image)
+        {
+            return ocrApi.GetTextFromImage(image);
+        }
+    }
+}
