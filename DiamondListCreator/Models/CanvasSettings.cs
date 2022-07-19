0bca41d8fa8f42e23304c9f6848a38cd7fc551b0
Alexander <alexander.ivashchenkoo@gmail.com>
2022-07-19 11:06:04 +0300

    Fixed the size name property for non standard sizes

--- DiamondListCreator/Models/CanvasSettings.cs
+++ DiamondListCreator/Models/CanvasSettings.cs
@@ -230,6 +230,8 @@ namespace DiamondListCreator.Models
                 : sizeWidth < sizeHeight ? ((int)pixelsInOneSm * sizeHeight) : oldSizeWidth;
 
             PageHeight += CanvasHeight - oldCanvasHeight;
+
+            SizeName += "+";
         }
     }
 }
