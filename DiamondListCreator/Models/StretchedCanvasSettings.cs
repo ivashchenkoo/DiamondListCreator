4b570c347ed6739fc7be8f41049f6e8b713f05d8
Alexander <alexander.ivashchenkoo@gmail.com>
2022-07-19 17:24:49 +0300

    Added stretched canvas settings model

---
+++ DiamondListCreator/Models/StretchedCanvasSettings.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace DiamondListCreator.Models
+{
+    public class StretchedCanvasSettings
+    {
+        [JsonProperty("Розмір")]
+        public string SizeName { get; set; }
+    }
+}
