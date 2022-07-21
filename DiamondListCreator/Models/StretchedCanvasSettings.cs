9f5bc9570de4260a158694b1defbadfc78aa71ad
Alexander <alexander.ivashchenkoo@gmail.com>
2022-07-21 14:09:38 +0300

    Changed some model properties

--- DiamondListCreator/Models/StretchedCanvasSettings.cs
+++ DiamondListCreator/Models/StretchedCanvasSettings.cs
@@ -82,23 +82,23 @@ namespace DiamondListCreator.Models
         [JsonProperty("Висота рамки")]
         public int BorderHeight { get; set; }
 
-        [JsonProperty("Відсуп зліва холста зі схеми")]
+        [JsonProperty("Відступ зліва холста зі схеми")]
         public int CanvasFromShemeMarginLeft { get; set; }
 
-        [JsonProperty("Відсуп зверху холста зі схеми")]
+        [JsonProperty("Відступ зверху холста зі схеми")]
         public int CanvasFromShemeMarginTop { get; set; }
 
         [JsonProperty("Ширина холста зі схеми")]
         public int CanvasFromShemeWidth { get; set; }
 
-        [JsonProperty("Розмір шрифту")]
-        public int FontSize { get; set; }
-
         [JsonProperty("Відступ збоку бокових елементів")]
         public int SideElementSideOffset { get; set; }
 
-        [JsonProperty("Відступ знизу бокових елементів")]
-        public int SideElementBottomOffset { get; set; }
+        [JsonProperty("Відступ від краю бокових елементів (вертикальна)")]
+        public int SideElementOffsetVertical { get; set; }
+
+        [JsonProperty("Відступ від краю бокових елементів (горизонтальна)")]
+        public int SideElementOffsetHorizontal { get; set; }
 
         [JsonProperty("Висота бокових елементів")]
         public int SideElementHeight { get; set; }
@@ -123,14 +123,26 @@ namespace DiamondListCreator.Models
             SizeHeight = stretchedCanvasSettings.SizeHeight;
             IsVertical = stretchedCanvasSettings.IsVertical;
             PageWidth = stretchedCanvasSettings.PageWidth;
+            PageWidth = stretchedCanvasSettings.PageWidth;
+            PageHeight = stretchedCanvasSettings.PageHeight;
             PageHeight = stretchedCanvasSettings.PageHeight;
             CanvasWidth = stretchedCanvasSettings.CanvasWidth;
+            CanvasWidth = stretchedCanvasSettings.CanvasWidth;
+            CanvasHeight = stretchedCanvasSettings.CanvasHeight;
             CanvasHeight = stretchedCanvasSettings.CanvasHeight;
             BorderWidth = stretchedCanvasSettings.BorderWidth;
             BorderHeight = stretchedCanvasSettings.BorderHeight;
             CanvasFromShemeMarginLeft = stretchedCanvasSettings.CanvasFromShemeMarginLeft;
             CanvasFromShemeMarginTop = stretchedCanvasSettings.CanvasFromShemeMarginTop;
             CanvasFromShemeWidth = stretchedCanvasSettings.CanvasFromShemeWidth;
+            SideElementSideOffset = stretchedCanvasSettings.SideElementSideOffset;
+            SideElementOffsetVertical = stretchedCanvasSettings.SideElementOffsetVertical;
+            SideElementOffsetHorizontal = stretchedCanvasSettings.SideElementOffsetHorizontal;
+            SideElementHeight = stretchedCanvasSettings.SideElementHeight;
+            MarginLeft = stretchedCanvasSettings.MarginLeft;
+            MarginTop = stretchedCanvasSettings.MarginTop;
+            MarginRight = stretchedCanvasSettings.MarginRight;
+            MarginBottom = stretchedCanvasSettings.MarginBottom;
         }
 
         public void SetSize(int sizeWidth, int sizeHeight)
@@ -160,8 +172,8 @@ namespace DiamondListCreator.Models
         {
             MarginLeft = (PageWidth - CanvasWidth) / 2;
             MarginTop = (PageHeight - CanvasHeight) / 2;
-            MarginRight = MarginLeft + CanvasWidth;
-            MarginBottom = MarginTop + CanvasHeight;
+            MarginRight = MarginLeft + CanvasWidth - 3;
+            MarginBottom = MarginTop + CanvasHeight - 3;
         }
     }
 }
